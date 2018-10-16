a1ffa34050718269d919c61b0e922cfc7341fd30
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-16 10:31:59 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,9 +11,10 @@ end
     #tweet.gsub!(pattern, dictionary)
 
 def word_substituter(tweet)
-  dictionary = {"hello" => 'hi', "to" => '2', "two" => '2', "too" => '2', "for" => '4', "four" => '4', 'be' => 'b', 'you' => 'u', "at" => "@", "and" => "&"}
+  dictionary
   keys1 = dictionary.keys
   tweet_in_array = tweet.split
+  
   tweet_in_array.each do |word|
     if keys1.include?(word.downcase)
       tweet_in_array[tweet_in_array.index(word)] = dictionary[word.downcase]
@@ -24,25 +25,13 @@ end.join(" ")
 end
 
 def bulk_tweet_shortener(tweet_array)
-  tweet_array.each do |tweet|
-    puts word_substituter(tweet)
-  end
+  tweet_array.each {|tweet| puts word_substituter(tweet)}
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.size > 140
-    word_substituter(tweet)
-  else
-    tweet
-  end
+  tweet.size > 140 ? word_substituter(tweet) : tweet
 end
 
 def shortened_tweet_truncator(tweet)
-  if tweet.size > 140
-    word_substituter(tweet)[0..136]+"..."
-  else
-    tweet
-  end
-  
-  
+  tweet.size > 140 ? word_substituter(tweet)[0..136]+"..." : tweet
 end
